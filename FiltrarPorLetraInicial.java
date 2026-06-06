9f40f09130ba5b1c14a59fee3d46935f82290683
Nicolas Prieto <nicolas.prieto-r@mail.escuelaing.edu.co>
2026-06-05 19:03:48 -0500

    Filtrar por letra inicial

---
+++ FiltrarPorLetraInicial.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+
+public class FiltrarPorLetraInicial {
+
+    List <String> nombres = List.of("Ana", "Carlos", "Andres", "Pedro", "Alejandra", "Juan", "Amanda");
+
+    public List<String> filtrarPorLetraInicial(){
+        return nombres.stream().filter(n -> n.startsWith("A")).toList();
+    }
+
+    public static void main(String[] args) {
+        
+        FiltrarPorLetraInicial ejercicio = new FiltrarPorLetraInicial();
+        ejercicio.filtrarPorLetraInicial();
+        
+        System.out.println(ejercicio.filtrarPorLetraInicial());   
+    }
+
+ 
+     
+}
